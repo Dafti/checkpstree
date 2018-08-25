f52baf6792c1832ca59e36c8a9cd1fd41d4bace6
Dafti <daniel.gracia-perez@cfa-afti.fr>
2018-08-25 10:40:42 +0200

    [Fix] Authors' names

--- plugin/checkpstree.py
+++ plugin/checkpstree.py
@@ -1,9 +1,9 @@
 # Volatility
 #
 # Authors
-# Tony <tony.gerard@cfa-afti.fr>
-# Francois Xavier <francois-xavier.babin@cfa-afti.fr>
-# Eric <eric.jouenne@cfa-afti.fr>
+# Tony Gerard <tony.gerard@cfa-afti.fr>
+# Francois-Xavier Babin <francois-xavier.babin@cfa-afti.fr>
+# Eric Jouenne <eric.jouenne@cfa-afti.fr>
 # Daniel Gracia Perez <daniel.gracia-perez@cfa-afti.fr>
 #
 # This file is part of Volatility.
